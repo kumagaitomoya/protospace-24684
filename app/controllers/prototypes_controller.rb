5946808e6342d810b7221beaf1f95648aeaa91be
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-10-26 09:51:48 +0900

    10261000修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:show,:index]
+  before_action :authenticate_user!, except: [:index, :show] 
 
   def index
     @prototypes = Prototype.all
@@ -8,7 +8,8 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = Comment.all
+    # @comments = Comment.all
+    @comments = @prototype.comments.includes(:user)
   end
 
   def new
